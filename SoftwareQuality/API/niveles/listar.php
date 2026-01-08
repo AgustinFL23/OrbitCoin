840ab8c3a080552502235aa00b6ba0979a2cbed3
AgustinFL23 <127245897+AgustinFL23@users.noreply.github.com>
2026-01-08 00:46:39 -0600

    Delete SoftwareQuality directory

--- SoftwareQuality/API/niveles/listar.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once '../db.php';
-header('Content-Type: application/json');
-
-$stmt = $pdo->query("SELECT Id, Nombre, Descripcion FROM Nivel");
-echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
